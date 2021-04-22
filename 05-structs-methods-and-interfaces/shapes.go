6b2da987f79caafcc63fb4895b7af17562c38151
Dobrica <dobrica@getpenta.com>
2021-04-22 09:03:31 +0200

    05 wip

---
+++ 05-structs-methods-and-interfaces/shapes.go
@@ -0,0 +1,14 @@
+package main
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(rec Rectangle) float64 {
+	return 2 * (rec.Width + rec.Height)
+}
+
+func Area(rec Rectangle) float64 {
+	return rec.Width * rec.Height
+}
