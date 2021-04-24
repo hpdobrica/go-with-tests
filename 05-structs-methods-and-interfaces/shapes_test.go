ca511a5f799123e16d7a351ae5121616459159d8
Dobrica <dobrica@getpenta.com>
2021-04-24 10:23:08 +0200

    structs and pointers done

--- 05-structs-methods-and-interfaces/shapes_test.go
+++ 05-structs-methods-and-interfaces/shapes_test.go
@@ -15,13 +15,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("calculate the area", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := Area(rectangle)
-		want := 72.0
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12.0, 6.0}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+
 }
---
+++ 06-pointers-and-errors/go.mod
@@ -0,0 +1,10 @@
+module pointers
+
+go 1.16
+
+require (
+	github.com/kisielk/errcheck v1.6.0 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
+	golang.org/x/tools v0.1.0 // indirect
+)
