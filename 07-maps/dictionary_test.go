cd888e3ba8b5eaf8f23431e9137b419594bd2d2e
Dobrica <dobrica@getpenta.com>
2021-05-02 08:14:39 +0200

    start with maps

--- 07-maps/dictionary_test.go
+++ 07-maps/dictionary_test.go
@@ -1,1 +1,23 @@
 package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	t.Run("retrieves a word", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a drill"}
+
+		got := dictionary.Search("test")
+		want := "this is just a drill"
+
+		assertStrings(t, got, want)
+
+	})
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Working through https://quii.gitbook.io/learn-go-with-tests
 
 
-Now at: https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/maps
+Now at: https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/maps#write-the-test-first-1
